a138e8098036fd33f43d2b44e84d8baee7a5c191
Michael Cook <michael.cook@techdept.co.uk>
2017-11-29 16:49:33 +0000

    Integrating Tome of the Unknown (the new NPM package)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { TomeOfTheUnknown } from 'tome-of-the-unknown';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [TomeOfTheUnknown]
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private tomeOfTheUnknown: TomeOfTheUnknown) {}
 }
