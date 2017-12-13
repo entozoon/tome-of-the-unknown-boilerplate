1d0af7af8ba059a4b4d4884c78dfe42ed1519cbf
Michael Cook <michael.cook@techdept.co.uk>
2017-12-13 12:56:24 +0000

    Hooking into Tome with a mind to having only local components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { TomeOfTheUnknown } from 'tome-of-the-unknown';
+// import { TomeOfTheUnknown } from 'tome-of-the-unknown';
+import { TomeService } from 'tome-of-the-unknown';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [TomeOfTheUnknown]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private tomeOfTheUnknown: TomeOfTheUnknown) {}
+  public articles: any;
+
+  constructor(private tomeOfTheUnknown: TomeService) {
+    tomeOfTheUnknown.getArticles().then(articles => {
+      this.articles = articles;
+      console.log(this.articles);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
+import { TomeModule } from 'tome-of-the-unknown';
+import { TomeService } from 'tome-of-the-unknown';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, TomeModule],
+  providers: [TomeService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
